6c76c306dcb5b833a782bb657bc7e8aa54ec5128
lnsunrmkv <Possibledust11@gmail.com>
2021-12-14 10:37:38 +0300

    Добавьте файлы проекта.

---
+++ 14122ispkotik/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14122ispkotik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Hello World!";
+            this.BackColor = Color.Black;
+            this.Width = 450;
+            this.Height = 450;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackgroundImageLayout = ImageLayout.Center;
+            this.BackgroundImage = Image.FromFile("C:\\lnsunrmkv\\Pictures\\kotik.jpg");
+        }
+    }
+}
